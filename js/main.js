ffbae6c9d43168c14b59b275f1a064ea1a7814bc
Artyom.Evsyukov <artyom.evsyukov@gmail.com>
2023-04-28 14:25:59 +0300

    pause

--- js/main.js
+++ js/main.js
@@ -14,9 +14,11 @@ $(function () {
 var swiper = new Swiper(".mySwiper", {
     pagination: {
         el: ".swiper-pagination",
-        dynamicBullets: true,
+        clickable: true,
+        // dynamicBullets: true,
     },
     autoplay: {
-        delay: 5000,
+        delay: 10000,
+        pauseOnMouseEnter: true,
     },
 });
